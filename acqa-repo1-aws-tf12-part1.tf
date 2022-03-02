bace8f0d4515078699ee565bd4f73f39689ba63c
Accurics Test Account <spatwardhan@tenable.com>
2022-03-02 19:20:17 +0000

    fix/look-for-inline-reviews

--- acqa-repo1-aws-tf12-part1.tf
+++ acqa-repo1-aws-tf12-part1.tf
@@ -66,7 +66,7 @@ resource "aws_security_group" "acqa-test-securitygroup1" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/24"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 }
 
